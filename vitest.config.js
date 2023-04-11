cb27621d121de727a42ec351da95a0946750b847
Geno Roupsky <geno@roupsky.name>
2023-04-11 22:15:24 +0300

    fixup! fixup! fix ci workflow and split codecov and coveralls as separate jobs

--- vitest.config.js
+++ vitest.config.js
@@ -3,6 +3,9 @@ import { configDefaults, defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     ...configDefaults.test,
+    coverage: {
+      reporter: ['lcov'],
+    },
     environment: 'node',
     clearMocks: true,
     mockReset: true,
